e4cf1eb079f38d5d31ea8d6675d8c7b3c3fd8769
jprx <36464332+jprx@users.noreply.github.com>
2025-06-08 17:37:14 -0400

    Add completions, key hints, and autoindent

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,60 @@
+-- Mixture of default blink config, kickstart nvim's config, and some custom stuff
+-- Completions are the auto suggestion things (I like those)
+-- Snippets are the preformatted chunks of code that it pastes for you (I do not like those)
+-- Therefore I am only installing Blink which is a completion engine, no snippets
+-- nvim-cmp is what people used to use but Blink is all the raage these days it seems
+-- see: https://cmp.saghen.dev/installation
+return {
+  'saghen/blink.cmp',
+
+  -- use a release tag to download pre-built binaries
+  version = '1.*',
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'default' },
+
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = { documentation = { auto_show = false } },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'buffer' },
+    },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "lua" },
+
+    -- Shows a signature help window while you type arguments for a function
+    signature = { enabled = true },
+  },
+  opts_extend = { "sources.default" }
+}
