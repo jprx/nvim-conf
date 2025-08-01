a7645bfdb9050027533bd93f0d344fe591694833
jprx <36464332+jprx@users.noreply.github.com>
2025-08-01 09:09:48 -0400

    gitsigns: enable line blame

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,6 +5,7 @@ return {
       require('gitsigns').setup()
       local api = require('gitsigns')
       vim.keymap.set("n", "<leader>gs", api.preview_hunk, { desc='Git Preview' })
+      api.toggle_current_line_blame(true)
     end
   },
 }
