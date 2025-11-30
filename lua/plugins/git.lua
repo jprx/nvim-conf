c7868445c7e423399dc17d2322d9fe5bcccd5a2a
jprx <36464332+jprx@users.noreply.github.com>
2025-11-29 22:18:16 -0500

    Remove blame delay

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,7 +2,11 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     config = function()
-      require('gitsigns').setup()
+      require('gitsigns').setup({
+        current_line_blame_opts = {
+          delay = 0,
+        },
+      })
       local api = require('gitsigns')
       vim.keymap.set("n", "<leader>gs", api.preview_hunk, { desc='Git Preview' })
       api.toggle_current_line_blame(true)
