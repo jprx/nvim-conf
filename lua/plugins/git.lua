c3666f71601155fa041954e187ee11483f56af4e
jprx <36464332+jprx@users.noreply.github.com>
2025-12-06 14:07:19 -0500

    Add ]g and [g for moving between git hunks

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -9,6 +9,8 @@ return {
       })
       local api = require('gitsigns')
       vim.keymap.set("n", "<leader>gs", api.preview_hunk, { desc='Git Preview' })
+      vim.keymap.set("n", "]g", api.next_hunk, { desc='Next git change' })
+      vim.keymap.set("n", "[g", api.prev_hunk, { desc='Prev git change' })
       api.toggle_current_line_blame(true)
     end
   },
