994b8eef90817ccd4ff6fe372b94f2a2321508bb
jprx <36464332+jprx@users.noreply.github.com>
2026-06-27 09:50:32 -0500

    Swap cmds for find word and open grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,8 @@ return {
     config = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<C-f>', builtin.grep_string, { desc = 'Telescope grep string' })
-      vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>f', builtin.grep_string, { desc = 'Telescope grep string' })
+      vim.keymap.set('n', '<C-f>', builtin.live_grep, { desc = 'Telescope live grep' })
     end
   },
   {
