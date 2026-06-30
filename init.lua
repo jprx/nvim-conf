5a8c23ef2c5770f8dca1417c949f7e0f4a9233f7
jprx <36464332+jprx@users.noreply.github.com>
2026-06-30 11:30:35 -0500

    Add sourcekit lsp

--- init.lua
+++ init.lua
@@ -21,5 +21,33 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
+vim.lsp.enable("sourcekit")
+vim.lsp.config("sourcekit", {
+  cmd = { 'sourcekit-lsp' },
+  filetypes = { 'swift' },
+  root_markers = {
+    '.git',
+    'compile_commands.json',
+    '.sourcekit-lsp',
+    'Package.swift',
+  },
+  get_language_id = function(_, ftype)
+    return ftype
+  end,
+  capabilities = {
+    workspace = {
+      didChangeWatchedFiles = {
+        dynamicRegistration = true,
+      },
+    },
+    textDocument = {
+      diagnostic = {
+        dynamicRegistration = true,
+        relatedDocumentSupport = true,
+      },
+    },
+  },
+})
+
 -- disable the vim splash screen when opening for the first time
 vim.cmd("set shortmess+=I")
